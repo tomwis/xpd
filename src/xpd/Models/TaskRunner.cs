ab5cb6c0edb7a100c856264569c805399a7b5e7d
Tomasz Wiśniewski <tomaszwisniewski00@gmail.com>
2024-10-10 19:20:05 +0200

    fix: added schema to TaskRunner class
    Without it $schema property in json was removed after editing.

--- src/xpd/Models/TaskRunner.cs
+++ src/xpd/Models/TaskRunner.cs
@@ -4,6 +4,9 @@ namespace xpd.Models;
 
 public class TaskRunner
 {
+    [JsonPropertyName("$schema")]
+    public string Schema { get; init; } = null!;
+
     [JsonPropertyName("tasks")]
     public List<TaskRunnerTask> Tasks { get; init; } = null!;
 }
